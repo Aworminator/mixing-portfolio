17bdbcf4b2f0bf2343063aee14a4cf1184d1f956
Aworminator <alec_garnica@yahoo.com>
2025-08-17 21:55:06 -0400

    Home Page CSS has been created and completed

---
+++ .history/script_20250817164914.js
@@ -0,0 +1,146 @@
+let songs = [
+  {
+    albumArt:
+      "https://facedownrecords.com/wp-content/uploads/2023/09/FCD195-3000.jpg",
+    artist: "American Arson",
+    song: "The Heat |",
+    name: "Run - American Arson.wav",
+    artistLink: "https://open.spotify.com/artist/7cl9j0U54gBxbcGO7zXXs2?si=Y8YFn_WdRy2WaXkD7kTK9A",
+    songLink: "https://open.spotify.com/track/4LscxvJWC9iHOBJmns7oVh?si=cf5a5d8cefe242de"
+  },
+  {
+    albumArt:
+      "https://i.scdn.co/image/ab67616d00001e02c0d49229ee1521c01ba40c3e",
+    artist: "Chery Blossom Club",
+    song: "Astray",
+    name: "All Is Well - Jared Withrow.wav",
+    artistLink: "",
+    songLink: "https://open.spotify.com/track/60aH38LMrZZam1xIewtDBR?si=670e49b3f44149bb",
+  },
+  {
+    albumArt:
+      "https://i.scdn.co/image/ab67616d00001e02066f1a88ce4c1492f016dc33",
+    artist: "Jared Withrow",
+    song: "All Is Well",
+    name: "All Is Well - Jared Withrow.wav",
+    artistLink: "",
+    songLink: "https://open.spotify.com/track/60aH38LMrZZam1xIewtDBR?si=670e49b3f44149bb",
+  },
+  {
+    albumArt:
+      "https://scontent-det1-1.xx.fbcdn.net/v/t39.30808-6/345889822_3156030747876824_1292364129699861349_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=f9_qdRFkm2IQ7kNvwFt4FJf&_nc_oc=AdmAUkniDUuNAhV0EcdJR1OdZMpTkVkt2HKyPtUgQ5yV5tYitW3uvtS0a4ZDftt4at6ejXA5V_AI77aQpg6WEzSH&_nc_zt=23&_nc_ht=scontent-det1-1.xx&_nc_gid=XrT0jrxaK4GVetPSgd7OrA&oh=00_AfW3ozMtD7ki8JJeSMwEqo1o4N_opRzJL01Q5s7aF34L3g&oe=68A7EA3A",
+    artist: "Camino Kid",
+    song: "Fireflies(Cover)",
+    name: "Fireflies - Camino Kid.wav",
+    artistLink: "",
+    songLink: "https://open.spotify.com/track/60aH38LMrZZam1xIewtDBR?si=670e49b3f44149bb",
+  },
+  {
+    albumArt:
+      "https://i.scdn.co/image/ab67616d0000b27369a57afe6b668fe34d4e9914",
+    artist: "Philip Bowen",
+    song: "Better Way",
+    name: "Fireflies - Camino Kid.wav",
+    artistLink: "",
+    songLink: "https://open.spotify.com/track/60aH38LMrZZam1xIewtDBR?si=670e49b3f44149bb",
+  },
+  {
+    albumArt:
+      "https://www.shazam.com/mkimage/image/thumb/Music122/v4/09/7e/41/097e41aa-4970-2849-a3a6-7e0279f900af/artwork.jpg/375x375bb.webp",
+    artist: "The Scally Brothers",
+    song: "Lion and the Lamb",
+    name: "Fireflies - Camino Kid.wav",
+    artistLink: "",
+    songLink: "https://open.spotify.com/track/60aH38LMrZZam1xIewtDBR?si=670e49b3f44149bb",
+  },
+];
+
+let input = document.querySelector("#input");
+
+function updateProgress(e, songName) {
+  let { currentTime, duration } = e.srcElement;
+  const progressPercentage = (currentTime / duration) * 100;
+  document.getElementById(
+    `progress-${songName}`
+  ).style.width = `${progressPercentage}%`;
+
+  const playBtn = document.getElementById(`playBtn-${songName}`);
+  if (currentTime === duration) {
+    playBtn.classList.add("fa-play");
+    playBtn.classList.remove("fa-pause");
+  } else {
+    playBtn.classList.add("fa-pause");
+    playBtn.classList.remove("fa-play");
+  }
+}
+
+function setProgress(e, songName) {
+  const width = e.currentTarget.clientWidth;
+  const clickX = e.offsetX;
+  const audio = document.getElementById(`audio-${songName}`);
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+function playAudio(songName) {
+  const audio = document.getElementById(`audio-${songName}`);
+  audio.play();
+}
+
+function pauseAudio(songName) {
+  const audio = document.getElementById(`audio-${songName}`);
+  audio.pause();
+}
+
+songs.forEach((song) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+    <img class="album-artwork" src="${song.albumArt}" alt="">
+    <div class="mp3">
+      <div class="band-info">
+        <a href="${song.link}" class="link"><h1 class="band-name">${song.song}</h1></a>
+        <a href="" class="link"><h2 class="album-title">${song.artist}</h2></a>
+      </div>
+      <div class="player">
+        <i class="fa-solid fa-play" id="playBtn-${song.name}" onclick="toggleAudio('${song.name}')"></i>
+        <audio preload="metadata" id="audio-${song.name}" src="./audio/${song.name}"></audio>
+        <div class="progress-bar" id="progress-bar-${song.name}">
+          <div class="progress" id="progress-${song.name}"></div>
+        </div>
+      </div>
+    </div>
+  `;
+  input.appendChild(card);
+
+  const audio = document.getElementById(`audio-${song.name}`);
+  const progressBar = document.getElementById(`progress-bar-${song.name}`);
+
+  audio.addEventListener("timeupdate", (e) => updateProgress(e, song.name));
+  progressBar.addEventListener("click", (e) => setProgress(e, song.name));
+});
+
+function toggleAudio(songName) {
+  const audio = document.getElementById(`audio-${songName}`);
+  const playBtn = document.getElementById(`playBtn-${songName}`);
+
+  if (audio.paused || audio.ended) {
+    playAudio(songName);
+    playBtn.classList.remove("fa-play");
+    playBtn.classList.add("fa-pause");
+  } else {
+    pauseAudio(songName);
+    playBtn.classList.remove("fa-pause");
+    playBtn.classList.add("fa-play");
+  }
+
+  audio.onpause = () => {
+    playBtn.classList.remove("fa-pause");
+    playBtn.classList.add("fa-play");
+  };
+  audio.onplay = () => {
+    playBtn.classList.remove("fa-play");
+    playBtn.classList.add("fa-pause");
+  };
+}
